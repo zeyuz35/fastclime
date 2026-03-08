1ee415a9324ea0aa5bc09b091ddf4bdedeba4234
zeyuz35 <19887504+zeyuz35@users.noreply.github.com>
2026-03-08 21:58:40 +0000

    Optimize matrix multiplications in dantzig() using crossprod

--- R/dantzig.R
+++ R/dantzig.R
@@ -14,8 +14,8 @@ dantzig <- function(X, y, lambda = 0.01, nlambda = 50) {
 
   message("compute X^TX and X^y")
 
-  X2 = t(X) %*% X
-  Xy = t(X) %*% y
+  X2 = crossprod(X) # Use crossprod(X) as a faster equivalent to t(X) %*% X
+  Xy = crossprod(X, y) # Use crossprod(X, y) as a faster equivalent to t(X) %*% y
 
   message("start recovering")
 
