82b63ebc339628b1969eb6a7ef9f1f33d6625aa9
zeyuz35 <19887504+zeyuz35@users.noreply.github.com>
2026-03-12 20:03:24 +0000

    refactor: optimize t(X) %*% X to crossprod(X) in dantzig.R

--- R/dantzig.R
+++ R/dantzig.R
@@ -14,8 +14,10 @@ dantzig <- function(X, y, lambda = 0.01, nlambda = 50) {
 
   message("compute X^TX and X^y")
 
-  X2 = t(X) %*% X
-  Xy = t(X) %*% y
+  # Equivalent: crossprod(X) is X' %*% X but faster
+  X2 = crossprod(X)
+  # Equivalent: crossprod(X, y) is X' %*% y but faster
+  Xy = crossprod(X, y)
 
   message("start recovering")
 
