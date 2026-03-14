38e5bdba1be2faa259a93fe0c8e0788a79e177e1
zeyuz35 <19887504+zeyuz35@users.noreply.github.com>
2026-03-14 20:12:03 +0000

    📦 Binder: Explicitly namespace igraph and MASS functions to reduce dependencies
    - Moves `igraph` and `MASS` from `Depends` to `Imports` in `DESCRIPTION` to avoid unnecessary package attachment.
    - Removes blanket `import()` statements for `igraph` and `MASS` from `NAMESPACE`.
    - Explicitly namespaces all corresponding function calls in `R/fastclime.generator.R` (`MASS::mvrnorm`, `igraph::graph.adjacency`, `igraph::layout.fruchterman.reingold`) and `R/fastclime.plot.R` (`igraph::graph.adjacency`, `igraph::layout.fruchterman.reingold`).

--- R/fastclime.generator.R
+++ R/fastclime.generator.R
@@ -138,7 +138,7 @@ fastclime.generator = function(
   omega = solve(sigma)
 
   # generate multivariate normal data
-  x = mvrnorm(n, rep(0, d), sigma)
+  x = MASS::mvrnorm(n, rep(0, d), sigma)
 
   sigmahat = cov(x) * (1 - 1 / n)
 
@@ -157,8 +157,8 @@ fastclime.generator = function(
       col = gray.colors(256),
       main = "Covariance Matrix"
     )
-    g = graph.adjacency(theta, mode = "undirected", diag = FALSE)
-    layout.grid = layout.fruchterman.reingold(g)
+    g = igraph::graph.adjacency(theta, mode = "undirected", diag = FALSE)
+    layout.grid = igraph::layout.fruchterman.reingold(g)
 
     fullfig[3] = plot(
       g,
@@ -216,8 +216,8 @@ plot.sim = function(x, ...) {
   )
   image(as.matrix(x$theta), col = gray.colors(256), main = "Adjacency Matrix")
   image(x$sigma, col = gray.colors(256), main = "Covariance Matrix")
-  g = graph.adjacency(x$theta, mode = "undirected", diag = FALSE)
-  layout.grid = layout.fruchterman.reingold(g)
+  g = igraph::graph.adjacency(x$theta, mode = "undirected", diag = FALSE)
+  layout.grid = igraph::layout.fruchterman.reingold(g)
 
   plot(
     g,
--- R/fastclime.plot.R
+++ R/fastclime.plot.R
@@ -20,8 +20,8 @@ fastclime.plot = function(
   }
   diag(G) = 0
   Matrix(G, sparse = TRUE)
-  g = graph.adjacency(as.matrix(G != 0), mode = "undirected", diag = FALSE)
-  layout.grid = layout.fruchterman.reingold(g)
+  g = igraph::graph.adjacency(as.matrix(G != 0), mode = "undirected", diag = FALSE)
+  layout.grid = igraph::layout.fruchterman.reingold(g)
 
   if (epsflag == TRUE) {
     postscript(
