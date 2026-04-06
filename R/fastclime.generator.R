a70672ac85710e5f01cd743c1448243d3d84d583
zeyuz35 <19887504+zeyuz35@users.noreply.github.com>
2026-04-06 21:13:56 +0000

    📦 Binder: Use explicit namespacing for MASS and igraph

--- R/fastclime.generator.R
+++ R/fastclime.generator.R
@@ -139,7 +139,7 @@ fastclime.generator = function(
   omega = solve(sigma)
 
   # generate multivariate normal data
-  x = mvrnorm(n, rep(0, d), sigma)
+  x = MASS::mvrnorm(n, rep(0, d), sigma)
 
   sigmahat = cov(x) * (1 - 1 / n)
 
@@ -160,8 +160,8 @@ fastclime.generator = function(
       col = gray.colors(256),
       main = "Covariance Matrix"
     )
-    g <- graph.adjacency(theta, mode = "undirected", diag = FALSE)
-    layout.grid <- layout.fruchterman.reingold(g)
+  g <- igraph::graph.adjacency(theta, mode = "undirected", diag = FALSE)
+  layout.grid <- igraph::layout.fruchterman.reingold(g)
 
     plot(
       g,
@@ -219,8 +219,8 @@ plot.sim = function(x, ...) {
   on.exit(par(old_par), add = TRUE)
   image(as.matrix(x$theta), col = gray.colors(256), main = "Adjacency Matrix")
   image(x$sigma, col = gray.colors(256), main = "Covariance Matrix")
-  g <- graph.adjacency(x$theta, mode = "undirected", diag = FALSE)
-  layout.grid <- layout.fruchterman.reingold(g)
+  g <- igraph::graph.adjacency(x$theta, mode = "undirected", diag = FALSE)
+  layout.grid <- igraph::layout.fruchterman.reingold(g)
 
   plot(
     g,
