11ba397cc6bc32a4d991b94496c033ebbdea1ac2
zeyuz35 <19887504+zeyuz35@users.noreply.github.com>
2026-04-10 21:10:43 +0000

    ⚙️ Modernizer: Replace t(X) %*% X with crossprod(X)

--- R/fastclime.ZKL15.R
+++ R/fastclime.ZKL15.R
@@ -32,7 +32,7 @@ fastclime.ZKL15 <- function(
   bigN <- ncol(X)
 
   if (is.null(Sigma)) {
-    Sigma <- (t(X) %*% X) / bigT
+    Sigma <- crossprod(X) / bigT  # Equivalent to t(X) %*% X but faster
   }
 
   diag_N <- diag(bigN)
