4391797d8e255c9a9dd19931e974c7b14c02c5da
zeyuz35 <19887504+zeyuz35@users.noreply.github.com>
2026-04-11 23:10:37 +0000

    ⚡ Bolt: Use crossprod for covariance matrix calculation

--- R/fastclime.BK17.R
+++ R/fastclime.BK17.R
@@ -30,7 +30,8 @@ fastclime.BK17 <- function(
   bigN <- ncol(X)
 
   if (is.null(Sigma)) {
-    Sigma <- (t(X) %*% X) / bigT
+    # Use crossprod for faster and more memory-efficient calculation
+    Sigma <- crossprod(X) / bigT
   }
 
   diag_N <- diag(bigN)
--- R/fastclime.ZKL15.R
+++ R/fastclime.ZKL15.R
@@ -32,7 +32,8 @@ fastclime.ZKL15 <- function(
   bigN <- ncol(X)
 
   if (is.null(Sigma)) {
-    Sigma <- (t(X) %*% X) / bigT
+    # Use crossprod for faster and more memory-efficient calculation
+    Sigma <- crossprod(X) / bigT
   }
 
   diag_N <- diag(bigN)
