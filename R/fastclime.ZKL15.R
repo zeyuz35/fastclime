9de3cf7fbd0e5f13d346364a04569e58d1c73a5a
zeyuz35 <19887504+zeyuz35@users.noreply.github.com>
2026-04-26 22:52:15 +0000

    Optimize symmetric matrix calculations
    Replaced `(t(X) %*% X)` with `crossprod(X)` and added `symmetric = TRUE`
    to `eigen()` in `fastclime.BK17.R` and `fastclime.ZKL15.R` to improve
    initialization performance.

--- R/fastclime.ZKL15.R
+++ R/fastclime.ZKL15.R
@@ -32,14 +32,14 @@ fastclime.ZKL15 <- function(
   bigN <- ncol(X)
 
   if (is.null(Sigma)) {
-    Sigma <- (t(X) %*% X) / bigT
+    Sigma <- crossprod(X) / bigT
   }
 
   diag_N <- diag(bigN)
 
   # Calculate penalty parameters if not provided
   sigma_x <- apply(X, 2, sd)
-  Rho <- eigen(Sigma, only.values = TRUE)$values
+  Rho <- eigen(Sigma, symmetric = TRUE, only.values = TRUE)$values
   rho_min <- max(min(Rho), 0.01)
   R <- 1 / rho_min
 
