e4954fb66eed8a51d4e2d6bd987bc8967228d522
zeyuz35 <19887504+zeyuz35@users.noreply.github.com>
2026-05-14 00:05:06 +0000

    ⚡ Bolt: Optimize covariance calculation with crossprod

--- R/fastclime.ZKL15.R
+++ R/fastclime.ZKL15.R
@@ -32,7 +32,8 @@ fastclime.ZKL15 <- function(
   bigN <- ncol(X)
 
   if (is.null(Sigma)) {
-    Sigma <- (t(X) %*% X) / bigT
+    # ⚡ Bolt: Optimize performance using native crossprod
+    Sigma <- crossprod(X) / bigT
   }
 
   diag_N <- diag(bigN)
