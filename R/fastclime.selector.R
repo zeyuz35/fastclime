942846d6747f6acd1bf6d7aec088635182eb0705
zeyuz35 <19887504+zeyuz35@users.noreply.github.com>
2026-05-15 20:59:21 +0000

    style: professionalize lambda warning message and sync tests

--- R/fastclime.selector.R
+++ R/fastclime.selector.R
@@ -49,7 +49,7 @@ fastclime.selector <- function(lambdamtx, icovlist, lambda) {
 
   if (status == 1) {
     warning(
-      "Some columns do not reach the required lambda!\nYou may want to increase lambda.min or use a larger nlambda."
+      "Some columns failed to reach the required lambda. Consider increasing lambda.min or using a larger nlambda."
     )
   }
 
