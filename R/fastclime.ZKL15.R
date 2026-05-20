4b3232ed25f7762d7b7791df9d4c10b1a49b82fd
zeyuz35 <19887504+zeyuz35@users.noreply.github.com>
2026-05-20 21:19:24 +0000

    ⚙️ Modernizer: crossprod optimization

--- R/fastclime.ZKL15.R
+++ R/fastclime.ZKL15.R
@@ -32,7 +32,8 @@ fastclime.ZKL15 <- function(
   bigN <- ncol(X)
 
   if (is.null(Sigma)) {
-    Sigma <- (t(X) %*% X) / bigT
+    # Optimization for performance: crossprod is faster than t(X) %*% X
+    Sigma <- crossprod(X) / bigT
   }
 
   diag_N <- diag(bigN)
