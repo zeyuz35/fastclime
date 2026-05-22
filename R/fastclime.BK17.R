ecb2e960b7d9d6b0af873b0f12c899582aaa0743
zeyuz35 <19887504+zeyuz35@users.noreply.github.com>
2026-05-22 23:19:13 +0000

    ⚡ Bolt: Optimize matrix multiplication with crossprod

--- R/fastclime.BK17.R
+++ R/fastclime.BK17.R
@@ -30,7 +30,8 @@ fastclime.BK17 <- function(
   bigN <- ncol(X)
 
   if (is.null(Sigma)) {
-    Sigma <- (t(X) %*% X) / bigT
+    # Bolt: Use crossprod(X) for faster and more memory-efficient matrix multiplication
+    Sigma <- crossprod(X) / bigT
   }
 
   diag_N <- diag(bigN)
