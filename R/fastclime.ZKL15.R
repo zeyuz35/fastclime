62d62ef6aa9e251f3132cdc313f1e2de1149eeb6
zeyuz35 <19887504+zeyuz35@users.noreply.github.com>
2026-05-24 21:21:42 +0000

    ⚙️ Modernizer: crossprod optimization

--- R/fastclime.ZKL15.R
+++ R/fastclime.ZKL15.R
@@ -32,7 +32,8 @@ fastclime.ZKL15 <- function(
   bigN <- ncol(X)
 
   if (is.null(Sigma)) {
-    Sigma <- (t(X) %*% X) / bigT
+    # Bolt: Optimize performance by using native crossprod() instead of t(X) %*% X
+    Sigma <- crossprod(X) / bigT
   }
 
   diag_N <- diag(bigN)
