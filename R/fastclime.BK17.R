dfe95ed408d75d9b4158fed0a705aca5086f6740
zeyuz35 <19887504+zeyuz35@users.noreply.github.com>
2026-06-06 23:31:36 +0000

    Optimize covariance matrix calculation using crossprod()

--- R/fastclime.BK17.R
+++ R/fastclime.BK17.R
@@ -28,9 +28,10 @@ fastclime.BK17 <- function(
   # preliminary
   bigT <- nrow(X)
   bigN <- ncol(X)
+  # Bolt: Use crossprod(X) instead of t(X) %*% X to avoid explicit matrix allocation
 
   if (is.null(Sigma)) {
-    Sigma <- (t(X) %*% X) / bigT
+    Sigma <- crossprod(X) / bigT
   }
 
   diag_N <- diag(bigN)
